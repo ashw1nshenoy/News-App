b91047feb3c212647b599df48469ddb9bd6bf2d0
Ashwin <ashwinshenoy20@gmail.com>
2023-11-27 19:56:51 +0530

    search completed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port=3000
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 const apiKey = '50db29f424a24bff9a17d8f8589dcba3';
-// app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('./public'))
 app.get('/hello',(req,res)=>{
   res.send("testing")
@@ -67,7 +67,7 @@ app.get('/', async (req, res) => {
    
   } catch (error) {
     console.error('Error fetching news:', error.message);
-    res.status(500).send('Internal Server Error');
+    res.status(500).render('error')
   }
 });
 app.get('/technology',async(req,res)=>{
@@ -121,7 +121,7 @@ app.get('/technology',async(req,res)=>{
  
   catch(error){
     console.error('Error fetching news:', error.message);
-    res.status(500).send('Not fetched');
+    res.status(500).render('error')
   }
 
 })
@@ -176,7 +176,7 @@ app.get('/business',async(req,res)=>{
 
   catch(error){
     console.error('Error fetching news:', error.message);
-    res.status(500).send('Internal Server Error');
+    res.status(500).render('error')
   }
 
 })
@@ -230,7 +230,7 @@ app.get('/science',async(req,res)=>{
   }
   catch(error){
     console.error('Error fetching news:', error.message);
-    res.status(500).send('Internal Server Error');
+    res.status(500).render('error')
   }
 
 })
@@ -284,7 +284,7 @@ app.get('/sports',async(req,res)=>{
   }
   catch(error){
     console.error('Error fetching news:', error.message);
-    res.status(500).send('Internal Server Error');
+    res.status(500).render('error')
   }
 
 })
@@ -338,13 +338,65 @@ app.get('/health',async(req,res)=>{
   }
   catch(error){
     console.error('Error fetching news:', error.message);
-    res.status(500).send('Internal Server Error');
+    res.status(500).render('error')
   }
 
 })
-app.post('/search',(req,res)=>{
-  const body=req.body
-  console.log(body)
+app.post('/search',async(req,res)=>{
+   try{
+    const searched=req.body.searched
+    console.log(searched)
+    const apiUrl = `https://newsapi.org/v2/everything?q=${searched}&apiKey=${apiKey}`
+    // console.log(apiUrl)
+    const response = await axios.get(apiUrl)
+    let data1={
+      img:response.data.articles[0].urlToImage,
+       title:response.data.articles[0].title,
+       description:response.data.articles[0].description,
+       time:response.data.articles[0].publishedAt,
+       author:response.data.articles[0].author
+     }
+     let data2={
+      img:response.data.articles[1].urlToImage,
+       title:response.data.articles[1].title,
+       description:response.data.articles[1].description,
+       time:response.data.articles[1].publishedAt,
+       author:response.data.articles[1].author
+     }
+     let data3={
+      img:response.data.articles[2].urlToImage,
+       title:response.data.articles[2].title,
+       description:response.data.articles[2].description,
+       time:response.data.articles[2].publishedAt,
+       author:response.data.articles[2].author
+     }
+     let data4={
+      img:response.data.articles[3].urlToImage,
+       title:response.data.articles[3].title,
+       description:response.data.articles[3].description,
+       time:response.data.articles[3].publishedAt,
+       author:response.data.articles[3].author
+     }
+     let data5={
+      img:response.data.articles[4].urlToImage,
+       title:response.data.articles[4].title,
+       description:response.data.articles[4].description,
+       time:response.data.articles[4].publishedAt,
+       author:response.data.articles[4].author
+     }
+     let data6={
+      img:response.data.articles[5].urlToImage,
+       title:response.data.articles[5].title,
+       description:response.data.articles[5].description,
+       time:response.data.articles[5].publishedAt,
+       author:response.data.articles[5].author
+     }
+     res.render('home',{data1: data1,data2:data2,data3:data3,data4:data4,data5:data5,data6:data6})
+    }
+  catch(error){
+    console.error('Error fetching news:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
 })
 // app.get('/search',async(req,res)=>{
 //   try{
